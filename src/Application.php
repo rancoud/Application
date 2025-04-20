4846a13ea48a42eb3834373cb8b6b053e9d526a4
Sébastien Rancoud <rancoud@users.noreply.github.com>
2025-04-20 03:04:30 +0200

    f

--- src/Application.php
+++ src/Application.php
@@ -121,7 +121,7 @@ class Application
             \ini_set('display_errors', '1');
         } else {
             \ini_set('display_errors', '0');
-            \error_reporting(\E_ALL & ~\E_NOTICE & ~\E_DEPRECATED & ~\E_STRICT & ~\E_USER_NOTICE & ~\E_USER_DEPRECATED);
+            \error_reporting(\E_ALL & ~\E_NOTICE & ~\E_DEPRECATED & ~\E_USER_NOTICE & ~\E_USER_DEPRECATED);
         }
     }
 
