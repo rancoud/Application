b99ab62ba37a3923cce746c020f6aa3461af7e57
Sébastien Rancoud <rancoud@users.noreply.github.com>
2025-04-21 13:08:19 +0200

    chore: clean files for PHP 8.4

--- tests/ImplementApplication.php
+++ tests/ImplementApplication.php
@@ -6,10 +6,16 @@ namespace tests;
 
 use Rancoud\Application\Application;
 
+/** @internal */
 class ImplementApplication extends Application
 {
-    public function convertMemoryLimitToBytes($memoryLimit)
+    public function convertMemoryLimitToBytes(string $memoryLimit): int
     {
         return parent::convertMemoryLimitToBytes($memoryLimit);
+    }
+
+    public function getMemoryPercentage(int $memoryUsage, string $memoryLimit): float
+    {
+        return parent::getMemoryPercentage($memoryUsage, $memoryLimit);
     }
 }
